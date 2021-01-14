5b64979ebdfa28b0e2e3b97f66ba35d2442e0a17
Mardoniosc <mardonio@live.com>
2021-01-14 11:47:49 -0300

    Feito INSERT E UPDATE
    Feito funções que realizam essas operações

--- 1 - Realtime/app.js
+++ 1 - Realtime/app.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * Váriaveis usadas durante o desenvolvimento
  */
 var CARD_CONTAINER = document.getElementsByClassName("card-container")[0];
@@ -14,6 +14,7 @@ var NOMES = [
   "Igor",
   "Joana",
 ];
+var ref = firebase.database().ref('card');
 
 /**
  * Botão para cria um card no card-contaier
@@ -33,8 +34,15 @@ function criarCard() {
    */
   firebase:
 
-  firebase.database().ref('card/' + card.nome).set(card).then(() => {
-    adicionaCardATela(card)
+  // ref.child(card.nome).set(card).then(() => {
+  //   adicionaCardATela(card)
+  // })
+
+  /**
+   * push() : Cria um id unico e insere os dados dentro desse uid
+   */
+  ref.push(card).then((snapshot) => {
+    adicionaCardATela(card, snapshot.key)
   })
 }
 
@@ -42,24 +50,108 @@ function criarCard() {
  * Recebe a referencia do card e exclui do banco de dados
  * @param {String} id Id do card
  */
-function deletar(id) {}
+function deletar(id) {
+  var card = document.getElementById(id);
+
+  /**
+   * .remove(): Remove o no em que o metodo é utilizado
+   * remove também todos os nos dentro desse nó removido
+   */
+  ref.child(id).remove().then(() => {
+    card.remove();
+  })
+
+
+  /**
+   * .set(null) : Ao setar um no em nulo exclui esse no do firebase
+   */
+  // ref.child(id).set(null).then(() => {
+  //   card.remove();
+  // });
+
+
+
+}
 
 /**
  * Incrementa o numero de curtidas
  * @param {String} id Id do card
  */
-function curtir(id) {}
+function curtir(id) {
+  var card = document.getElementById(id);
+  var count = card.getElementsByClassName('count-number')[0];
+  var countNumber = +count.innerText;
+  countNumber = countNumber + 1;
+
+  /**
+   * .set(): Pode ser acessado diretamente o objeto que quer atualizar
+   * e passar o valor atualizado ou pode-ser passar o objeto completo e
+   * atualiza-lo com os novos valores nos campos correspondentes
+   */
+  ref.child(id + '/curtidas').set(countNumber).then(() => {
+    count.innerText = countNumber;
+  })
+}
 
 /**
  * Decrementa o numero de curtidas
  * @param {String} id Id do card
  */
-function descurtir(id) {}
+function descurtir(id) {
+  var card = document.getElementById(id);
+  var count = card.getElementsByClassName('count-number')[0];
+  var countNumber = +count.innerText;
+  if(countNumber > 0){
+    countNumber = countNumber - 1;
+
+    /**
+     * update(): Recebe um objeto (e apenas um objeto)
+     * e atualiza APENAS as propriedades desse objeto
+     */
+    ref.child(id).update({curtidas: countNumber}).then(() => {
+      count.innerText = countNumber;
+    })
+  }
+
+
+}
 
 /**
  * Espera o evento de que a DOM está pronta para executar algo
  */
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  /**
+   * Once retorna os dados lidos de uma URL
+   * snapshot: objeto retornado pela leitura
+   */
+  ref.once('value').then(snapshot => {
+
+    // // acessa um no filho
+    // console.log('Child => ',snapshot.child('-MR0BxrfsmSTfqF9_sHU').val());
+
+    // // checa se exite algo no snapshot
+    // console.info('Exists() => ', snapshot.exists());
+
+    // // checa se exite filho passado na url
+    // console.info('hasChild() nome => ', snapshot.hasChild('-MR0BxrfsmSTfqF9_sHU/nome'));
+    // console.info('hasChild() comentario => ', snapshot.hasChild('-MR0BxrfsmSTfqF9_sHU/comentario'));
+
+    // // se exite algum filho no no
+    // console.log('hasChildren() => ', snapshot.child('-MR0BxrfsmSTfqF9_sHU'));
+
+    // // numro de filhos no snapshot
+    // console.log('numChildren => ', snapshot.numChildren());
+
+    // // a chave do snapshot/caminho
+    // console.log('Chave snapshot:', snapshot.key);
+
+
+    snapshot.forEach(value => {
+      // console.log('Chave:', value.key);
+      adicionaCardATela(value.val(), value.key);
+    });
+  })
+});
 
 /**
  * Adiciona card na tela
