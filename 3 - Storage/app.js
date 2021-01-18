4011d5ae34b2f89b5efe4428f7e6979296af7667
Mardoniosc <mardonio@live.com>
2021-01-18 11:04:11 -0300

    Upload de arquivo 'storage'

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -1,19 +1,42 @@
 /**
  * Variáveis com referencias dos inputs
  */
-var fileInput = document.getElementById('file-input');
-var stringInput = document.getElementById('string-input');
+var fileInput = document.getElementById("file-input");
+var stringInput = document.getElementById("string-input");
 
+/**
+ * Referencia para o storage do firebase criando uma pasta com o nome de arquivos
+ */
+var ref = firebase.storage().ref("arquivos");
 /**
  * Metodo que observa mudanças no input de arquivo
  */
 fileInput.onchange = function (event) {
-    
-}
+  var arquivo = event.target.files[0];
+
+  /**
+   * .child(nome) : Acessar o caminhjo para inserir o arquivo
+   * .put(arquivo) : Vai inserir o arquivo
+   */
+  ref
+    .child("arquivo")
+    .put(arquivo)
+    .then((snapshot) => {
+      console.log("Snapshot => ", snapshot);
+
+      /**
+       * .getDownloadURL() - Retorna a url para dowload/apresentação desse arquivo enviado
+       */
+      ref
+        .child("arquivo")
+        .getDownloadURL()
+        .then((url) => {
+          console.log('Download URL String => ', url);
+        });
+    });
+};
 
 /**
  * Metodo que observa mudanças no input de string
  */
-stringInput.onchange = function (event) {
-
-}
+stringInput.onchange = function (event) {};
