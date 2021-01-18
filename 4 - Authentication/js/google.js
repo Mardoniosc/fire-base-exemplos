ec21fd920dc5efc23f6a625d6e4a1438049eaee9
Mardoniosc <mardonio@live.com>
2021-01-18 15:52:51 -0300

    Login com facebook e google

--- 4 - Authentication/js/google.js
+++ 4 - Authentication/js/google.js
@@ -1,3 +1,14 @@
 function loginGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
 
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((response) => {
+      console.log(`Usuario ${response.user}`);
+      console.log("Token => ", response.credential.accessToken);
+    })
+    .catch((err) => {
+      console.log("Erro =>", err);
+    });
 }
