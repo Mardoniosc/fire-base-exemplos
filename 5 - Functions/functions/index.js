f9e54425dd265b7da20686d7432ae1e3fb6fe480
Mardoniosc <mardonio@live.com>
2021-01-19 10:58:56 -0300

    Adicionando um card pelo functions

--- 5 - Functions/functions/index.js
+++ 5 - Functions/functions/index.js
@@ -1,24 +1,27 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp({
-    apiKey: "AIzaSyAVZ3tJyT6j3wNzRA2WhTeeDer1SGuIOAU",
-    authDomain: "curso-firebase-app-85b96.firebaseapp.com",
-    projectId: "curso-firebase-app-85b96",
-    databaseURL: "https://curso-firebase-app-85b96-default-rtdb.firebaseio.com",
-    storageBucket: "curso-firebase-app-85b96.appspot.com",
-    messagingSenderId: "865939655633",
-    appId: "1:865939655633:web:292a3ce71512cfcca56ef0",
-    measurementId: "G-N5RSQ498RQ"
+  apiKey: "AIzaSyAVZ3tJyT6j3wNzRA2WhTeeDer1SGuIOAU",
+  authDomain: "curso-firebase-app-85b96.firebaseapp.com",
+  projectId: "curso-firebase-app-85b96",
+  databaseURL: "https://curso-firebase-app-85b96-default-rtdb.firebaseio.com",
+  storageBucket: "curso-firebase-app-85b96.appspot.com",
+  messagingSenderId: "865939655633",
+  appId: "1:865939655633:web:292a3ce71512cfcca56ef0",
+  measurementId: "G-N5RSQ498RQ",
 });
 
 exports.addCard = functions.https.onRequest((request, response) => {
-    let card = JSON.parse(request.body);
+  let card = JSON.parse(request.body);
 
-    admin.database().ref('card').push(card).then(() => {
-        response.status(200).send('Gravação realizada com sucesso');
-    }, error => {
+  admin.database().ref("card").push(card).then(
+      () => {
+        response.status(200).send("Gravação realizada com sucesso");
+      },
+      (error) => {
         response.status(500).send(error);
-    });
+      }
+    );
 });
 
 /**
@@ -27,21 +30,21 @@ exports.addCard = functions.https.onRequest((request, response) => {
  * .onDelete = ao excluir um dado em um nó
  * .onWrite = ao executar qualquer uma das funções anteriores
  */
-exports.updateCount = functions.database.ref('/card/{pushId}').onCreate((snapshot, context) => {
-    // .onceCreate((snapshot, context)): snapshot é o dado atual / contexto da chamada.
-    admin.database().ref('card').once('value').then(snap => {
-        admin.database().ref('contagem').set(snap.numChildren()).then(() => {
-            // é preciso retornar um dado ou uma promessa
-            return snap.numChildren();
-        })
-    });
-});
+// exports.updateCount = functions.database.ref('/card/{pushId}').onCreate((snapshot, context) => {
+//     // .onceCreate((snapshot, context)): snapshot é o dado atual / contexto da chamada.
+//     admin.database().ref('card').once('value').then(snap => {
+//         admin.database().ref('contagem').set(snap.numChildren()).then(() => {
+//             // é preciso retornar um dado ou uma promessa
+//             return snap.numChildren();
+//         })
+//     });
+// });
 
-exports.updateName = functions.firestore.document('/cards/{userId}').onCreate((snapshot, context) => {
-    let nome = snapshot.data().nome;
-    nome = nome.toUpperCase();
+// exports.updateName = functions.firestore.document('/cards/{userId}').onCreate((snapshot, context) => {
+//     let nome = snapshot.data().nome;
+//     nome = nome.toUpperCase();
 
-    admin.firestore().collection('cards').doc(snapshot.id).update({nome}).then(() => {
-        return nome;
-    });
-})
+//     admin.firestore().collection('cards').doc(snapshot.id).update({nome}).then(() => {
+//         return nome;
+//     });
+// })
